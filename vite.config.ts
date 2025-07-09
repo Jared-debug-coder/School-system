d3db41f58468326e827efb4a4676b18628866f62
Jared-debug-coder <ombongijared2@Gmail.com>
2025-07-09 12:59:01 +0300

    Clean up: Remove all deployment configurations and files

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { writeFileSync } from "fs";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/',
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    {
-      name: 'add-nojekyll',
-      writeBundle() {
-        writeFileSync('dist/.nojekyll', '');
-      }
-    }
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
