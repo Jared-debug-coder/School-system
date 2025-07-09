e86b5099dc0d32479df94f7f209e7e8560285320
Jared-debug-coder <ombongijared2@Gmail.com>
2025-07-09 11:25:58 +0300

    Update base URL for renamed repository

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/shulepro-kenyan-schools/' : '/',
+  base: mode === 'production' ? '/School-System/' : '/',
   server: {
     host: "::",
     port: 8080,
